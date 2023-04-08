df14bfcced9f31d34bc1b5c72a85431e3255231f
efren.diaz <efren.diaz@lemonade.com>
2023-04-08 10:44:30 -0500

    Create DepartmentDto

---
+++ department-service/src/main/java/com/learningspring/departmentservice/model/dto/DepartmentDto.java
@@ -0,0 +1,16 @@
+package com.learningspring.departmentservice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDto {
+    public String code;
+    public String name;
+    public String description;
+}
