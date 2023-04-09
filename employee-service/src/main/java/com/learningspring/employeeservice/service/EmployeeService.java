3b91c44224be0a1c0c6972621c0a1cc7cea1fd8c
efren.diaz <efren.diaz@lemonade.com>
2023-04-09 07:44:03 -0500

    Create EmployeeService

---
+++ employee-service/src/main/java/com/learningspring/employeeservice/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.learningspring.employeeservice.service;
+
+import com.learningspring.employeeservice.model.dto.EmployeeDto;
+import com.learningspring.employeeservice.model.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface EmployeeService {
+
+    public Employee createEmployee(EmployeeDto employeeDto);
+
+    public List<Employee> getAllEmployees();
+
+    public Employee getEmployeeById(long id);
+
+    public Employee updateEmployee(long id, EmployeeDto employeeDto);
+
+    public String deleteEmployee(long id);
+}
---
+++ employee-service/src/main/java/com/learningspring/employeeservice/service/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.learningspring.employeeservice.service;
+
+import com.learningspring.employeeservice.model.dto.EmployeeDto;
+import com.learningspring.employeeservice.model.entity.Employee;
+import com.learningspring.employeeservice.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+
+    @Override
+    public Employee createEmployee(EmployeeDto employeeDto) {
+        Employee employee = Employee.builder()
+                .firstName(employeeDto.getFirstName())
+                .lastName(employeeDto.getLastName())
+                .email(employeeDto.getEmail())
+                .build();
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Employee with ID '%d' NOT FOUND", id)));
+    }
+
+    @Override
+    public Employee updateEmployee(long id, EmployeeDto employeeDto) {
+        Employee employee = getEmployeeById(id);
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setEmail(employeeDto.getEmail());
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public String deleteEmployee(long id) {
+        Employee employee = getEmployeeById(id);
+        employeeRepository.delete(employee);
+        return String.format("Employee with ID '%d' has been deleted successfully", id);
+    }
+}
