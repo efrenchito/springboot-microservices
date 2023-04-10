bf022eb02568a8170f567dd4cef08ee6d948b237
efren.diaz <efren.diaz@lemonade.com>
2023-04-09 21:31:57 -0500

    Create custom ErrorDetails ~ (department-service)

---
+++ department-service/src/main/java/com/learningspring/departmentservice/exception/ErrorDetails.java
@@ -0,0 +1,15 @@
+package com.learningspring.departmentservice.exception;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String description;
+}
