58466a52efdb38d8654f66220cf6b67d91974326
efren.diaz <efren.diaz@lemonade.com>
2023-04-10 08:15:15 -0500

    Add handleMethodArgumentNotValidException to GlobalExceptionHandler ~ (employee-service)

--- employee-service/src/main/java/com/learningspring/employeeservice/exception/GlobalExceptionHandler.java
+++ employee-service/src/main/java/com/learningspring/employeeservice/exception/GlobalExceptionHandler.java
@@ -2,18 +2,24 @@ package com.learningspring.employeeservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails>  handleGlobalException(Exception exception, WebRequest webRequest) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(), exception.getClass() + " " + exception.getMessage(), webRequest.getDescription(false)
+        );
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
@@ -23,5 +29,19 @@ public class GlobalExceptionHandler {
                 new Date(), exception.getMessage(), webRequest.getDescription(false)
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest webRequest) {
+        Map<String, String> mapErrors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            mapErrors.put(field, message);
+        });
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(), mapErrors.toString(), webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
