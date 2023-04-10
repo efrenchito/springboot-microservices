1146b7c5148fb9a1fcaf991b8a9b6ea8279dc012
efren.diaz <efren.diaz@lemonade.com>
2023-04-10 08:09:38 -0500

    Apply validation to Dto bean ~ (employee-service)

--- employee-service/src/main/java/com/learningspring/employeeservice/model/dto/EmployeeDto.java
+++ employee-service/src/main/java/com/learningspring/employeeservice/model/dto/EmployeeDto.java
@@ -3,6 +3,8 @@ package com.learningspring.employeeservice.model.dto;
 import lombok.*;
 
 import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
@@ -10,7 +12,12 @@ import javax.persistence.Column;
 @AllArgsConstructor
 @Builder
 public class EmployeeDto {
+
+    @NotEmpty(message = "firstName can NOT be empty")
     private String firstName;
+    @NotEmpty(message = "lastName can NOT be empty")
     private String lastName;
+    @NotEmpty(message = "email can NOT be empty")
+    @Email(message = "email should have a valid format")
     private String email;
 }
