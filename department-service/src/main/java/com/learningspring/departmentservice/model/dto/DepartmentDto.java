f41c99c56d7767d8e235c8134c5f06477957dd13
Jose Vasquez <jose.vasquez@metromile.com>
2023-05-18 07:59:51 -0500

    Add anotations to schema

--- department-service/src/main/java/com/learningspring/departmentservice/model/dto/DepartmentDto.java
+++ department-service/src/main/java/com/learningspring/departmentservice/model/dto/DepartmentDto.java
@@ -1,5 +1,7 @@
 package com.learningspring.departmentservice.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,14 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "Department model information")
 public class DepartmentDto {
 
+    @ApiModelProperty(value = "Code used internally")
     @NotBlank(message = "code field can NOT be empty")
     public String code;
+    @ApiModelProperty(value = "Official name")
     public String name;
+    @ApiModelProperty(value = "Ful description of department")
     public String description;
 }
