12961fd60c888dedad15948b2c58ddf3904d69e0
Jose Vasquez <jose.vasquez@metromile.com>
2023-05-18 07:56:56 -0500

    Add apiInfo

---
+++ department-service/src/main/java/com/learningspring/departmentservice/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.learningspring.departmentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+            "Spring boot department API",
+                "Description...",
+                "1.0",
+                "tos-url",
+                "jous32@gmail.om",
+                "License",
+                "license-url"
+        );
+    }
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
